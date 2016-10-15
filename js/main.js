cee206ca55e89747a51b7f5ae8518a51b7bbf940
terrentinomcpickle <dmrjp.777@gmail.com>
2016-10-15 15:11:48 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $("#bastard").click(function(e){
     var firstNameA = ["Terri", "Terre", "Terro", "Tele", "Tech", "Titty", "Toots", "Teen", "Tear", "Tella", "Tackle", "Trance", "Turn", "Turtle", "Tetra", "Tweako"];
     var firstNameB = ["tosh", "fuck", "ass", "train", "anus", "mance", "scoots", "rave", "pence", "pants", "tubby", "box", "adactyl", "duck", "vibe", "moncule", "crotch", "dance"];
 
-    var lastNameA = ["McPickle", "McDongle", "McMushroom", "McAss", "McDildo", "McBoner", "McFuck", "McScrotum", "McNBOMB", "McStonedApe", "McDingDong", "McSmart"];
+    var lastNameA = ["McPickle", "McDongle", "McMushroom", "McAss", "McDildo", "McBoner", "McFuck", "McScrotum", "McNBOMB", "McStonedApe", "McDingDong", "McSmart", "McDog"];
     var lastNameB = ["Shit", "Buttlicker", "Fuck", "DiMiTri", "Cunt", "Queef", "Bartard", "Face", "Antenna", "Testicle"];
 
     var name = "";
